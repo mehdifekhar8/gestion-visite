6076478a833e4a0d4653b19d0e078e60c66c5336
mahdi fekhar <mahdi.fekhar@cultofcoder.com>
2022-05-31 19:07:46 +0200

    get current location when adding a visit

--- microservices/admin/src/bundles/UIAppBundle/components/Map/AddMarker.tsx
+++ microservices/admin/src/bundles/UIAppBundle/components/Map/AddMarker.tsx
@@ -1,11 +1,14 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import Geocode from "react-geocode";
+import * as Ant from "antd";
 
 type Props = {
   onChange: (val: Coordinates) => void;
   minHeight?: string;
   width?: string;
+  isEditable?: boolean;
+  show?: boolean;
 };
 type Coordinates = {
   lat: number;
@@ -15,6 +18,8 @@ export const AddMarker: React.FunctionComponent<Props> = ({
   onChange,
   minHeight = "50vh",
   width = "100%",
+  isEditable = true,
+  show = true,
 }) => {
   const [currentPosition, setCurrentPosition] = useState<Coordinates>();
   const [selectedPosition, setSelectedPosition] = useState<Coordinates>();
@@ -30,38 +35,50 @@ export const AddMarker: React.FunctionComponent<Props> = ({
     };
 
     setCurrentPosition(currentPosition);
+    if (isEditable == false) {
+      console.log(currentPosition);
+      if (onChange) onChange(currentPosition);
+    }
   };
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success);
+    navigator.geolocation.getCurrentPosition((position) => {
+      success(position);
+    });
   }, [navigator.geolocation]);
-
   const onSelectedChange = (item: google.maps.MapMouseEvent) => {
-    setSelectedPosition({ lat: item.latLng.lat(), lng: item.latLng.lng() });
-    Geocode.fromLatLng(
-      item.latLng.lat().toString(),
-      item.latLng.lng().toString()
-    ).then(
-      (response) => {
-        const address = response.results[0]["address_components"];
-        setAddress(
-          "Address: " +
-            address[address.length - 2].long_name +
-            ", " +
-            address[address.length - 3].long_name +
-            ", " +
-            address[address.length - 4].long_name
-        );
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-    onChange({ lat: item.latLng.lat(), lng: item.latLng.lng() });
+    if (isEditable) {
+      setSelectedPosition({ lat: item.latLng.lat(), lng: item.latLng.lng() });
+      Geocode.fromLatLng(
+        item.latLng.lat().toString(),
+        item.latLng.lng().toString()
+      ).then(
+        (response) => {
+          const address = response.results[0]["address_components"];
+          setAddress(
+            "Address: " +
+              address[address.length - 2].long_name +
+              ", " +
+              address[address.length - 3].long_name +
+              ", " +
+              address[address.length - 4].long_name
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+      onChange({ lat: item.latLng.lat(), lng: item.latLng.lng() });
+    } else {
+      Ant.notification.warn({
+        message: "Not allowed",
+        description: "You are not allowed to modify current location",
+      });
+    }
   };
 
   return (
-    <Fragment>
+    <div style={show ? { display: "block" } : { display: "none" }}>
       <LoadScript googleMapsApiKey="AIzaSyBa9CtZ1XtdrQePGh1WRwpvBXWBUAN2pFQ">
         <GoogleMap
           mapContainerStyle={{ minHeight, width }}
@@ -78,7 +95,7 @@ export const AddMarker: React.FunctionComponent<Props> = ({
           {selectedPosition && <Marker position={selectedPosition} />}
         </GoogleMap>
       </LoadScript>
-      <h4 style={{marginTop: "14px"}}>{address} </h4>
-    </Fragment>
+      <h4 style={{ marginTop: "14px" }}>{address} </h4>
+    </div>
   );
 };
--- microservices/admin/src/bundles/UIAppBundle/pages/VisitsManagement/config/VisitCreateForm.tsx
+++ microservices/admin/src/bundles/UIAppBundle/pages/VisitsManagement/config/VisitCreateForm.tsx
@@ -1,11 +1,31 @@
 import { Service } from "@bluelibs/core";
 import { VisitCreateForm as BaseVisitCreateForm } from "./VisitCreateForm.base";
+import * as Ant from "antd";
+import { AddMarker } from "@bundles/UIAppBundle/components/Map/AddMarker";
+import { useState } from "react";
 
 @Service({ transient: true })
 export class VisitCreateForm extends BaseVisitCreateForm {
   build() {
     super.build();
-
+    this.update("coordinates", {
+      label: "Show Map",
+      required: false,
+      render: (props: { onChange: () => void; value: string } & any) => {
+        return (
+          <div style={{ display: "none" }}>
+            {" "}
+            <Ant.Form.Item name="body" {...props}>
+              <AddMarker
+                show={false}
+                isEditable={false}
+                onChange={props.onChange}
+              />
+            </Ant.Form.Item>
+          </div>
+        );
+      },
+    });
     // Perform additional modifications such as updating rendering functions, labels, description
   }
 }
