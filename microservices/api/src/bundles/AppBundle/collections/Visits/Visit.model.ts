060f32b7b7d161eece0be5d23fba40b575409011
mehdifekhar8 <75302515+mehdifekhar8@users.noreply.github.com>
2022-06-03 14:49:31 +0200

    changes

--- microservices/api/src/bundles/AppBundle/collections/Visits/Visit.model.ts
+++ microservices/api/src/bundles/AppBundle/collections/Visits/Visit.model.ts
@@ -1,7 +1,7 @@
 export * from "./Visit.model.base";
 import { Schema, Is, a, an } from "@bluelibs/validator-bundle";
 import { Visit as BaseVisit } from "./Visit.model.base";
-export { VisitProfile } from "./Visit.model.base";
+export {Visit as VisitProfile } from "./Visit.model.base";
 
 @Schema()
 export class Visit extends BaseVisit {
